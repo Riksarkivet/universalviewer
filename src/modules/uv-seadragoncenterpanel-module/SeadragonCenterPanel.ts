11ef1db0efc3a3266fc32ba26dad6f168cb110f1
Sebastian Öberg <sebastian.oberg@gmail.com>
2015-11-18 13:21:14 +0100

    Title length should also take into account the padding.

--- src/modules/uv-seadragoncenterpanel-module/SeadragonCenterPanel.ts
+++ src/modules/uv-seadragoncenterpanel-module/SeadragonCenterPanel.ts
@@ -592,7 +592,7 @@ class SeadragonCenterPanel extends CenterPanel {
         }
 
         if (this.$title.css("position") === "absolute")
-            this.$title.width(this.$viewer.width() - parseInt(this.$title.css("left"), 10));
+            this.$title.width(this.$viewer.width() - parseInt(this.$title.css("left"), 10) - parseInt(this.$title.css("padding-left"), 10) - parseInt(this.$title.css("padding-right"), 10));
 
         this.$title.ellipsisFill(this.title);
 
