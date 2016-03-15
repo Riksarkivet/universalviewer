76112bc1ff71c6fedbae0bb9ee8ff232534d5d66
Magnus Johansson <magnus.johansson@riksarkivet.se>
2016-03-15 11:59:10 +0100

    Changed the way to handle bildid update

--- src/modules/uv-shared-module/Riksarkivet.ts
+++ src/modules/uv-shared-module/Riksarkivet.ts
@@ -1,8 +1,15 @@
+import IMetadataItem = require("../uv-shared-module/IMetadataItem");
 class Riksarkivet {
     public GetBildIdFromCanvas(canvas: Manifesto.ICanvas) {
         var bildid = canvas.getImages()[0].getResource().getServices()[0].id;
-        bildid = bildid.substr(bildid.indexOf("!") + 1);
-
+        var datasource = bildid.substr(bildid.lastIndexOf("/") + 1).split('!')[0];
+        if (datasource == "arkis") {
+            datasource = "";
+        }
+        else {
+            datasource = datasource.substr(0, 1).toUpperCase() + datasource.substr(1) + "_";
+        }
+        bildid = datasource + bildid.substr(bildid.indexOf("!") + 1);
         return bildid;
     }
 }
